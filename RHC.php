179e875d7a59ab41a1a2e95c15d2f0d4a4bdc6c8
matzr75 <matthias@reinagl.com>
2017-03-30 15:03:28 +0200

    added Navigation pane in

--- RHC.php
+++ RHC.php
@@ -4,7 +4,34 @@
 </head>
 <body>
 <div id="Navigation">
-	<?php include("RHC_Navigation.php");?>
+	<?php
+	#include("RHC_Navigation.php");
+	echo"<form action='OHlog.php' method='post'>";
+        // Setup Database Connection
+        if (!class_exists('myDB')) {include "DBConnect.php";}
+        $db2 = new myDB("openHAB");
+        if (!is_null($db2) ) {
+                $result = mysqli_query($db2->con,"SELECT ItemName, ItemId FROM Items ORDER BY ItemName");
+                $list = array();
+	}
+	echo "<table>
+		<tr><td><button name='PV' type='button' onclick='this.form.submit();'>Photovoltaik Übersicht</button>
+		</td></tr>
+		<tr><td>OpenHAB Log:<br>
+        		<select name='OH_item' onchange='this.form.submit();'>
+	        	<option value=''>Auswahl...</option>";
+		
+        		while($row = mysqli_fetch_array($result))
+        		{
+               			echo "<option value=".$row['ItemId'].">".$row['ItemName']."</option>";
+         		}
+        		echo"</select>";
+		echo "</td></tr>
+		<tr><td><button name='Status' type='button' onclick='this.form.submit();'>Server Status</button>
+		</td></tr>
+		</table>";
+	echo"</form>";	
+	?>
 </div>
 <div id="Content">
 
