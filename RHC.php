2240aa43185e72298762fc056eca8d0e01f2256d
matzr75 <matthias@reinagl.com>
2017-03-30 14:00:13 +0200

    create file

--- RHC.php
+++ RHC.php
@@ -1,1 +1,14 @@
+<html>
+<head>
+<link href="Reinagl.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="Navigation">
+	<?php include("RHC_Navigation.php");?>
+</div>
+<div id="Content">
 
+</div>
+
+</body>
+</html>
