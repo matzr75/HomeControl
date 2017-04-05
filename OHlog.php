298813bba425d2049c126c64a4a311f7e34b6d11
matzr75 <matz@MacBook-Pro.local>
2017-04-05 21:13:21 +0200

    bugfix

--- OHlog.php
+++ OHlog.php
@@ -10,7 +10,7 @@
 <div id="Content">-->
 <?php
 echo "Hello World<br>";
-echo "item = " . $_POST["item"]
+echo "item = " . $_POST["item"];
 if (isset($_POST["item"]))
 {
 	$table = "Item" . $_POST["item"];
