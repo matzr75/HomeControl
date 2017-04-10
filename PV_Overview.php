6fc904475edf94e06e531906e8451598408e4b06
matzr75 <matthias@reinagl.com>
2017-04-10 22:53:52 +0200

    finalised table

--- PV_Overview.php
+++ PV_Overview.php
@@ -5,65 +5,65 @@ if (!class_exists('myDB')) {include "DBConnect.php";}
 $db = new myDB("PV");
 
 if (!is_null($db) ) {
+	$result = mysqli_fetch_row(mysqli_query($db->con, "SELECT max(DateTime) FROM T_PowerLog"));
+	echo "<div style='text-align: right; font-size: 75%; font-style:italic'>last Update: " . $result[0];
+
+	$result_week = mysqli_fetch_row(mysqli_query($db->con, "SELECT 'Diese Woche', sum(Stromverbrauch), sum(Produktion), sum(Einspeisung), sum(Eigenverbrauch), sum(Einsparung_Eigennutzung), sum(Ertrag_Einspeisung) FROM V_OVERVIEW_Income WHERE day >= curdate() - INTERVAL DAYOFWEEK(curdate())-2 DAY"));
+	$result_month = mysqli_fetch_row(mysqli_query($db->con, "SELECT 'Dieser Monat', sum(Stromverbrauch), sum(Produktion), sum(Einspeisung), sum(Eigenverbrauch), sum(Einsparung_Eigennutzung), sum(Ertrag_Einspeisung) FROM  V_OVERVIEW_Income where month(day) = month(curdate())"));
+	
 	$orderField = "day";
 	$orderDir = "DESC";
-	$sql = "
-		SELECT day,
-			Stromverbrauch,
-			Produktion,
-			Einspeisung,
-			Eigenverbrauch, 
-			(	select RateValue 
-				from T_Rates 
-				where RateType = 'Ertrag_je_kWH' 
-					and DateFrom <= t1.day 
-					and (DateTo >= t1.day OR DateTo is NULL)
-			)*Einspeisung/1000 as Ertrag_Einspeisung, 
-			(
-				select RateValue 
-				from T_Rates 
-				where RateType = 'Kosten_je_kWH' 
-					and DateFrom <= t1.day 
-					and (DateTo >= t1.day OR DateTo is NULL)
-			)*Eigenverbrauch/1000 as Ertrag_Eigennutzung 
-		FROM `V_OVERVIEW` t1
-		ORDER BY " . $orderField . " " . $orderDir;
-	echo $sql;
-	echo $result = mysqli_query($db->con, $sql);
-	$list = array();
-echo $result;
+	$sql = "SELECT day, Stromverbrauch, Produktion, Einspeisung, Eigenverbrauch, Ertrag_Einspeisung, Einsparung_Eigennutzung FROM V_OVERVIEW_Income ORDER BY " . $orderField . " " . $orderDir;
+	if( !($result = mysqli_query($db->con, $sql))) {
+		echo "Query FAILED !!!";
+	} 
+
 	echo "
 	<table>
 	<tr>
 		<th>Tag</th>
-		<th>Verbrauch</th>
-		<th>Produktion</th>
-		<th>Einspeisung</th>
-		<th>Eigenverbrauch</th>
-		<th>Ertrag Eingennutzung</th>
-		<th>Ertrag Einspeisung</th>
-<!--		<th>Gesamtertrag</th>-->
-	</tr>";
+		<th>Verbrauch<br>[kWh]</th>
+		<th>Produktion<br>[kWh]</th>
+		<th>Einspeisung<br>[kWh]</th>
+		<th>Eigenverbrauch<br>[kWh]</th>
+		<th>Ertrag Eingennutzung<br>[€]</th>
+		<th>Ertrag Einspeisung<br>[€]</th>
+		<th>Gesamtertrag<br>[€]</th>
+	</tr>
 
-	echo "<tr><td>1</td>";
-	echo "<td>2</td>";
-	echo "<td>3</td>";
-	echo "<td>4</td>";
-	echo "<td>5</td>";
-	echo "<td>6</td>";
-	echo "<td>7</td></tr>";
+	<tr>
+		<td style='text-align: left'>$result_week[0]</td>
+		<td style='text-align: right'>" . number_format($result_week[1]/1000, 1) . "</td>
+		<td style='text-align: right'>" . number_format($result_week[2]/1000, 1) . "</td>
+		<td style='text-align: right'>" . number_format($result_week[3]/1000, 1) . "</td>
+		<td style='text-align: right'>" . number_format($result_week[4]/1000, 1) . "</td>
+		<td style='text-align: right'>" . number_format($result_week[5], 2) . "</td>
+		<td style='text-align: right'>" . number_format($result_week[6], 2) . "</td>
+		<td style='text-align: right'>" . number_format(($result_week[5] + $result_week[6]), 2) . "</td>
+	</tr>
+	<tr>
+		<td style='text-align: left'>$result_month[0]</td>
+		<td style='text-align: right'>" . number_format($result_month[1]/1000, 1) . "</td>
+		<td style='text-align: right'>" . number_format($result_month[2]/1000, 1) . "</td>
+		<td style='text-align: right'>" . number_format($result_month[3]/1000, 1) . "</td>
+		<td style='text-align: right'>" . number_format($result_month[4]/1000, 1) . "</td>
+		<td style='text-align: right'>" . number_format($result_month[5], 2) . "</td>
+		<td style='text-align: right'>" . number_format($result_month[6], 2) . "</td>
+		<td style='text-align: right'>" . number_format(($result_month[5] + $result_month[6]), 2) . "</td>
+	</tr>
+	<tr><td colspan='8'></td></tr>";
+
 	while($row = mysqli_fetch_array($result))
 	{
-	//	$List[$row['Time']] = $row['value'];
 		echo "<tr>";
 			echo "<td>" . $row['day'] . "</td>";
-			echo "<td>" . $row['Stromverbrauch'] . "</td>";
-			echo "<td>" . $row['Produktion'] . "</td>";
-			echo "<td>" . $row['Einspeisung'] . "</td>";
-			echo "<td>" . $row['Eigenverbrauch'] . "</td>";
-			echo "<td>" . $row['Ertrag_Eigennutzung'] . "</td>";
-			echo "<td>" . $row['Ertrag_Einspeisung'] . "</td>";
-//			echo "<td>" . ($row['Ertrag_Eingennutzung'] + $row['Ertrag_Einspeisung']) . "</td>";
+			echo "<td style='text-align: right'>" . number_format($row['Stromverbrauch']/1000, 1) . "</td>";
+			echo "<td style='text-align: right'>" . number_format($row['Produktion']/1000, 1) . "</td>";
+			echo "<td style='text-align: right'>" . number_format($row['Einspeisung']/1000, 1) . "</td>";
+			echo "<td style='text-align: right'>" . number_format($row['Eigenverbrauch']/1000, 1) . "</td>";
+			echo "<td style='text-align: right'>" . number_format($row['Einsparung_Eigennutzung'], 2) . "</td>";
+			echo "<td style='text-align: right'>" . number_format($row['Ertrag_Einspeisung'], 2) . "</td>";
+			echo "<td style='text-align: right'>" . number_format(($row['Einsparung_Eigennutzung'] + $row['Ertrag_Einspeisung']), 2) . "</td>";
 		echo "</tr>";
 	}
 
